2a1012026a62fd335896958b14941fda85139da3
Daniel Loman <dloman@toyon.com>
2015-01-31 02:49:18 -0800

    Got game code working with networking code

--- FiestaMonsterz.py
+++ FiestaMonsterz.py
@@ -15,20 +15,43 @@ def Quit():
   exit()
 
 ################################################################################
+class FiestaMonsterz():
+  ##############################################################################
+  def __init__(self):
+    ScreenShape = (2200, 1600)
+    self.Window = pygame.display.set_mode(ScreenShape)
+    pygame.display.set_caption("Window")
+    self.MainWorld = World(ScreenShape)
+    self.MainWorld.AddEntity(Wall(0, 0, 0, 1600))
+    self.MainWorld.AddEntity(Wall(1, 0, 2200, 0))
+
+    self.MainWorld.AddEntity(\
+      LoopingFaceRock(self.MainWorld, self.Window, 2200, 1000, -15))
+
+    self.MainWorld.AddEntity(\
+      Platform(self.Window, 1100, 1064, 'ZigzagGrass_', 33))
+
+  ##############################################################################
+  def AddMonster(self, Id):
+    self.MainWorld.AddEntity(JumpMonster(self.Window, 1100, 300), Id)
+
+  ##############################################################################
+  def Update(self):
+    self.Window.fill(gBlack)
+    self.MainWorld.Update()
+    pygame.display.flip()
+
+  ##############################################################################
+  def AddEvent(self, Id, Event):
+    self.MainWorld.AddEventToQueue(Id, Event)
+
+################################################################################
 ################################################################################
 if __name__ == "__main__":
   pygame.init()
-  ScreenShape = (2200, 1600)
-  Window = pygame.display.set_mode(ScreenShape)
-  pygame.display.set_caption("Window")
   Clock = pygame.time.Clock()
-  MainWorld = World(ScreenShape)
-  MainWorld.AddEntity(Wall(0, 0, 0, 1600))
-  MainWorld.AddEntity(Wall(1, 0, 2200, 0))
-  MainWorld.AddEntity(LoopingFaceRock(MainWorld, Window, 2200, 1000, -15))
-  MainWorld.AddEntity(Platform(Window, 1100, 1064, 'ZigzagGrass_', 33))
-  MainWorld.AddEntity(JumpMonster(Window, 1100, 300), 0)
-
+  FiestaMonster = FiestaMonsterz()
+  FiestaMonster.AddMonster(0)
   while True:
     for Event in pygame.event.get():
       if (Event.type==pygame.QUIT):
@@ -37,9 +60,7 @@ if __name__ == "__main__":
         if Event.key == pygame.K_ESCAPE or Event.key == 113:
           Quit()
         elif (Event.key==pygame.K_UP):
-          MainWorld.AddEventToQueue(0, 'jump')
-    Window.fill(gBlack)
-    MainWorld.Update()
+          FiestaMonster.AddEvent(0, 'Jump')
     Clock.tick(50)
-    pygame.display.flip()
+    FiestaMonster.Update()
 
--- GameServer.py
+++ GameServer.py
@@ -3,37 +3,35 @@ import sys
 from twisted.internet import reactor, protocol
 from twisted.internet.task import LoopingCall
 from socket import *
-#from MonsterParty import MonsterParty
+
+from FiestaMonsterz import FiestaMonsterz
 
 ################################################################################
 ################################################################################
-class Echo(protocol.Protocol):
-    """This is just about the simplest possible protocol"""
+class GameServer(protocol.Protocol):
+  ##############################################################################
+  def connectionMade(self):
+    self.factory.Game.AddMonster(self)
 
-    def dataReceived(self, data):
-      print 'data = ', data
+  ##############################################################################
+  def dataReceived(self, Data):
+    self.factory.Game.AddEvent(self, Data)
 
 ################################################################################
 ################################################################################
-class Game():
-
-  def __init__(self):
-    self.mCounter = 0
-
-  def Update(self):
-    self.mCounter += 1
-    if self.mCounter > 100000:
-      print 'Doing game stuff'
-      self.mCounter = 0
+class GameServerFactory(protocol.ServerFactory):
+  ##############################################################################
+  def __init__(self, Game):
+    self.Game = Game
 
 ################################################################################
 ################################################################################
 if __name__ == '__main__':
-  MainGame = Game()
-  GameLoopingCall = LoopingCall(MainGame.Update)
+  FiestaMonster = FiestaMonsterz()
+  GameLoopingCall = LoopingCall(FiestaMonster.Update)
   GameLoopingCall.start(1/60) #60 fps
 
-  factory = protocol.ServerFactory()
-  factory.protocol = Echo
+  factory = GameServerFactory(FiestaMonster)
+  factory.protocol = GameServer
   reactor.listenTCP(42069, factory)
   reactor.run()
